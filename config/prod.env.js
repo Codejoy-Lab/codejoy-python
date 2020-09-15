e40bff40b7fbdb6556ea8863708500d1a1884a88
Siyu <paula555@outlook.com>
2020-09-15 19:33:29 +1000

    change api url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST:'"https://www.learn.codejoyai.com:18082"',
+  API_HOST:'"https://api.learn.codejoyai.com"',
   HOST:'"https://www.learn.codejoyai.com/codejoy-python/:9527"'
 }
